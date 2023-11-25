265c2086f11a4dbdc7761f921f13e2f12f4f925f
Bogdăneanu Daria-Maria <122162735+kaserolly@users.noreply.github.com>
2023-11-25 14:14:51 +0200

    Update Event.cpp

--- MyProject/MyProject/Event.cpp
+++ MyProject/MyProject/Event.cpp
@@ -6,44 +6,54 @@ Event::Event() : name(""), date(""), time("") {}
 Event::Event(const std::string& name, const std::string& date, const std::string& time)
     : name(name), date(date), time(time) {}
 
-std::string Event::getName() const {
+std::string Event::getName() const
+{
     return name;
 }
 
-std::string Event::getDate() const {
+std::string Event::getDate() const
+{
     return date;
 }
 
-std::string Event::getTime() const {
+std::string Event::getTime() const
+{
     return time;
 }
 
-void Event::displayInfo() const {
+void Event::displayInfo() const
+{
     std::cout << "Event: " << name << "\nDate: " << date << "\nTime: " << time << "\n";
 }
 
-Event::Event(const Event& other) {
+Event::Event(const Event& other)
+{
     copyFrom(other);
 }
 
-Event& Event::operator=(const Event& other) {
-    if (this != &other) {
+Event& Event::operator=(const Event& other)
+{
+    if (this != &other)
+    {
         clear();
         copyFrom(other);
     }
     return *this;
 }
 
-Event::~Event() {
+Event::~Event()
+{
     clear();
 }
 
-std::ostream& operator<<(std::ostream& out, const Event& event) {
+std::ostream& operator<<(std::ostream& out, const Event& event)
+{
     out << "Event: " << event.getName() << "\nDate: " << event.getDate() << "\nTime: " << event.getTime() << "\n";
     return out;
 }
 
-std::istream& operator>>(std::istream& in, Event& event) {
+std::istream& operator>>(std::istream& in, Event& event)
+{
     std::cout << "Enter event name: ";
     in >> event.name;
     std::cout << "Enter event date: ";
@@ -53,49 +63,66 @@ std::istream& operator>>(std::istream& in, Event& event) {
     return in;
 }
 
-Event::operator bool() const {
+Event::operator bool() const
+{
     // Implement explicit cast to bool based on relevant fields
     return !name.empty() && !date.empty() && !time.empty();
 }
 
-bool Event::operator<(const Event& other) const {
+bool Event::operator<(const Event& other) const
+{
     // Implement less than operator based on relevant fields
-    if (date != other.date) {
+    if (date != other.date)
+    {
         return date < other.date;
-    } else if (time != other.time) {
+    }
+    else if (time != other.time)
+    {
         return time < other.time;
-    } else {
+    }
+    else
+    {
         return name < other.name;
     }
 }
 
-bool Event::operator>(const Event& other) const {
+bool Event::operator>(const Event& other) const
+{
     // Implement greater than operator based on relevant fields
-    if (date != other.date) {
+    if (date != other.date)
+    {
         return date > other.date;
-    } else if (time != other.time) {
+    }
+    else if (time != other.time)
+    {
         return time > other.time;
-    } else {
+    }
+    else
+    {
         return name > other.name;
     }
 }
 
-bool Event::operator<=(const Event& other) const {
+bool Event::operator<=(const Event& other) const
+{
     // Implement less than or equal to operator based on relevant fields
     return !(*this > other);
 }
 
-bool Event::operator>=(const Event& other) const {
+bool Event::operator>=(const Event& other) const
+{
     // Implement greater than or equal to operator based on relevant fields
     return !(*this < other);
 }
 
-bool Event::operator==(const Event& other) const {
+bool Event::operator==(const Event& other) const
+{
     // Implement equality operator based on relevant fields
     return name == other.name && date == other.date && time == other.time;
 }
 
-void Event::copyFrom(const Event& other) {
+void Event::copyFrom(const Event& other)
+{
     name = other.name;
     date = other.date;
     time = other.time;

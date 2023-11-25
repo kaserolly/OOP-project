f457262812b901afa1a749c61011aa159c0274c0
Bogdăneanu Daria-Maria <122162735+kaserolly@users.noreply.github.com>
2023-11-25 14:22:05 +0200

    Update Ticket.cpp

--- MyProject/MyProject/Ticket.cpp
+++ MyProject/MyProject/Ticket.cpp
@@ -7,54 +7,61 @@ Ticket::Ticket() : id(nextId++), type("") {}
 
 Ticket::Ticket(const std::string& type) : id(nextId++), type(type) {}
 
-int Ticket::getId() const {
+int Ticket::getId() const
+{
     return id;
 }
 
-std::string Ticket::getType() const {
+std::string Ticket::getType() const
+{
     return type;
 }
 
-void Ticket::displayInfo() const {
+void Ticket::displayInfo() const
+{
     std::cout << "Ticket ID: " << id << "\nType: " << type << "\n";
 }
 
-Ticket::Ticket(const Ticket& other) {
+Ticket::Ticket(const Ticket& other)
+{
     copyFrom(other);
 }
 
-Ticket& Ticket::operator=(const Ticket& other) {
-    if (this != &other) {
+Ticket& Ticket::operator=(const Ticket& other)
+{
+    if (this != &other)
+    {
         clear();
         copyFrom(other);
     }
     return *this;
 }
 
-Ticket::~Ticket() {
+Ticket::~Ticket()
+{
     clear();
 }
 
-std::ostream& operator<<(std::ostream& out, const Ticket& ticket) {
+std::ostream& operator<<(std::ostream& out, const Ticket& ticket)
+{
     out << "Ticket ID: " << ticket.getId() << "\nType: " << ticket.getType() << "\n";
     return out;
 }
 
-std::istream& operator>>(std::istream& in, Ticket& ticket) {
+std::istream& operator>>(std::istream& in, Ticket& ticket)
+{
     std::cout << "Enter ticket type: ";
     in >> ticket.type;
     return in;
 }
 
-bool Ticket::operator==(const Ticket& other) const {
+bool Ticket::operator==(const Ticket& other) const
+{
     return id == other.id && type == other.type;
 }
 
-void Ticket::copyFrom(const Ticket& other) {
+void Ticket::copyFrom(const Ticket& other)
+{
     id = other.id;
     type = other.type;
-}
-
-void Ticket::clear() {
-    // Perform cleanup for dynamically allocated resources if any
 }

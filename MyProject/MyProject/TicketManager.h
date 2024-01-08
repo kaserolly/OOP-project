19e44d06ec6c13414c9ff18152f83ffec7079ccf
Bogdăneanu Daria-Maria <122162735+kaserolly@users.noreply.github.com>
2024-01-08 22:41:27 +0200

    Create TicketManager.h

---
+++ MyProject/MyProject/TicketManager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Location.h"
+#include "Event.h"
+#include "Ticket.h"
+#include <vector>
+
+class TicketManager {
+private:
+    std::vector<Ticket> issuedTickets;
+    Location location;
+    Event event;
+
+public:
+    TicketManager(const Location& location, const Event& event);
+
+    void generateTickets(int numTickets, const std::string& category);
+    void displayTickets() const;
+    bool validateTicket(const std::string& ticketId) const;
+
+     void saveTicketsToFile(const std::string& filename) const;
+    void loadTicketsFromFile(const std::string& filename);
+
+    void displayMenu() const;
+    void processUserChoice(int choice);
+};
